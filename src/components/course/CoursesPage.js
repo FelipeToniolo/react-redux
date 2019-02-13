7fdae9488d407b77df0ea700c2c535affc99f3d9
Felipe <ftoniolo.aat@gmail.com>
2019-02-13 16:17:23 -0200

    Atualizando Formulario ManageCourse

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,25 +1,35 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
-  constructor(props, context){
+  constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
-courseRow(course, index) {
-  return <div key={index}>{course.title}</div>;
-}
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
 
   render() {
-    const { courses} = this.props;
+    const { courses } = this.props;
 
     return (
       <div>
         <h1>Courses</h1>
-        <CourseList courses={courses}/>
+        <input type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
       </div>
     );
   }
@@ -36,10 +46,10 @@ function mapStateToProps(state, owbProps) {
   };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
