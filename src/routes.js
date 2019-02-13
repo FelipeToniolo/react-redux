7474cc65c5f277cffbf2860e4d218f49f70df2d7
Felipe <ftoniolo.aat@gmail.com>
2019-02-13 12:22:13 -0200

    Formulario ManageCourse
    fazer alteração preencher campos.

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,9 @@ import ManageCoursePage from './components/course/ManageCoursePage';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
-    <Route path="about" component={AboutPage}/>
+    <Route path="courses" component={CoursesPage}/>
     <Route path="course" component={ManageCoursePage}/>
     <Route path="course/:id" component={ManageCoursePage}/>
-    <Route path="courses" component={CoursesPage}/>
+    <Route path="about" component={AboutPage}/>
   </Route>
 );
